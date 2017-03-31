76cd90cad199ba00acbaebaf006551f947f4a7bd
dwaggenspack <dwaggenspack@stpao.org>
2017-03-31 15:00:12 -0500

    a bit o tweaks

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@
     var currentLatLng = {
         latlng: L.latLng(38.0307, -84.5040)
     };
-    var tiles = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    var tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://cartodb.com/attributions">Carto</a>',
         subdomains: 'abcd',
         minZoom: 3
     });
